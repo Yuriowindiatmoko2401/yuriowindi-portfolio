339c9b52f39a13abd3942c9f3b31cf17a5a15fdb
Yuriowindiatmoko2401 <yuriowindiatmoko@gmail.com>
2025-03-27 13:56:27 +0700

    Add admin-login layout file to fix build error

---
+++ src/app/admin-login/layout.js
@@ -0,0 +1,8 @@
+export const metadata = {
+  title: 'Admin Login',
+  description: 'Portfolio Admin Login Page',
+};
+
+export default function AdminLoginLayout({ children }) {
+  return children;
+}
